febb7034a5f081230ff93925981630af7e7f6222
Tlotliso19 <tlotlisomakoboshane917@gmail.com>
2024-09-09 10:26:27 +0200

    added three files for the bjects and sqlite

---
+++ habit_objects.py
@@ -0,0 +1,144 @@
+''' on this module to record all object related code'''
+
+##important modules 
+import sqlite3 # for data recording 
+import calendar # for time and take 
+from datetime import datetime,timedelta # for time and take
+import pickle # for serialization 
+
+
+'''our first class to handle all habit related code '''
+'''class Habits: 
+    highest_streaks = {} # to hold the highest streaks as a dictionery
+
+    def __init__(self, name, frequency):
+        self.name = name
+        self.frequency = frequency
+        self.streak = 0
+
+    def __repr__(self):
+        return f"Habits(name={self.name}, frequency={self.frequency}, streak={self.streak})"
+
+    def update_streak(self):
+        if self.name in self.__class__.highest_streaks: #when there is the streak 
+            #then lets check if its higher than our current one 
+            if self.streak > self.__class__.highest_streaks[self.name]:
+                self.__class__.highest_streaks[self.name] = self.streak
+        else:
+            #when it is not there at all lets add it
+            self.__class__.highest_streaks[self.name] = self.streak'''
+#slidely revised 
+class Habits_101:
+    highest_streaks = {}
+
+    def __init__(self, name, frequency):
+        self.name = name
+        self.frequency = frequency
+        self.streak = 0
+
+    def __repr__(self):
+        return f"Habits(name={self.name}, frequency={self.frequency}, streak={self.streak})"
+
+'''the second class that will handle the good habits code'''
+'''class Good_habits(Habits):
+    streaks_times = {}
+    
+    def __init__(self, name, frequency):
+        super().__init__(name, frequency)
+        self.year = datetime.now().year
+        self.month = datetime.now().month
+        self.num_days = calendar.monthrange(self.year, self.month)[1]
+        # Initialize the performed time for this habit if it doesn't exist
+        if name not in self.__class__.streaks_times:
+            self.__class__.streaks_times[name] = None
+
+    def display_data(self):
+        target = self.num_days / self.frequency
+        current = self.streak
+        return current, target
+
+    def update_streak(self):
+        if self.performed_on_time():
+            self.streak += 1 #firstly lets update the streak 
+            # secondly lets update the last perfomance time 
+            self.__class__.streaks_times[self.name] = datetime.now()
+        else:
+           self.streak =0 #this will be set to zero
+           self.__class__.streaks_times[self.name] = datetime.now() #this will remain the same 
+
+    def performed_on_time(self):
+        # Check if the habit was performed within the allowed frequency
+        last_streak_time = self.__class__.streaks_times.get(self.name, None)
+        if last_streak_time:
+            if (datetime.now() - last_streak_time) > timedelta(days=self.frequency):
+                return True
+        else:
+            return True
+        return False
+
+    def performed(self):
+       #check if the habit has been performed 
+        last_streak_time = self.__class__.streaks_times.get(self.name, None)
+        if last_streak_time:
+            if (datetime.now() - last_streak_time) > timedelta(days=self.frequency):
+                return True
+        else:
+            return False
+        return False'''
+    
+    #slidely improvements 
+
+class Good_habits_101(Habits_101):
+    streaks_times = {}
+
+    def __init__(self, name, frequency):
+        super().__init__(name, frequency)
+        self.year = datetime.now().year #keeping track of time 
+        self.month = datetime.now().month
+        self.num_days = calendar.monthrange(self.year, self.month)[1]
+        if name not in self.__class__.streaks_times:
+            self.__class__.streaks_times[name] = None #adding it to the dictionery if it is not present at initialiazation
+
+    def perform(self): #when we perform the habit
+        if self.performed():
+            self.update_streak()
+            print(f"Habit '{self.name}' performed successfully. Streak updated.")
+        else:
+            print(f"Habit '{self.name}' was not performed within the allowed frequency.")
+
+    def display_data(self): #for vidualizing 
+        target = self.num_days / self.frequency
+        current = self.streak
+        return current, target
+
+    def update_streak(self):
+        if self.performed():
+
+            self.streak += 1 #updating the streak
+            print('has been run 1')
+        else:
+          print('has been run 2')
+          self.streak += 0  
+
+    def performed(self):
+       
+        current_time = datetime.now()
+        last_streak_time = self.__class__.streaks_times.get(self.name, None)
+
+        # Update the last performed time weather it is performed all not 
+        self.__class__.streaks_times[self.name] = current_time    
+        if last_streak_time is None: #then valid    
+            return True
+        # If last performance was within the allowed frequency
+        time_defference =(current_time - last_streak_time) < timedelta(days=self.frequency)
+        if time_defference:
+            return True
+        
+        return False
+    
+'''for some reasons they are needed here '''
+def serialize_object(obj):
+    return pickle.dumps(obj)
+
+def deserialize_object(serialized_data):
+    return pickle.loads(serialized_data)
---
+++ sqlite_101.py
@@ -0,0 +1,76 @@
+'''module to handle all sqlite related functions '''
+##important modules 
+import sqlite3 # for data recording 
+import calendar # for time and take 
+from datetime import datetime # for time and take
+import pickle # for serialization 
+
+
+# Connect to SQLite database (or create it if it doesn't exist)
+
+#making it a function 
+def database_connect():
+    conn = sqlite3.connect('my_database.db')
+    cursor = conn.cursor()
+
+    # Create a table to store serialized objects
+    cursor.execute('''
+    CREATE TABLE IF NOT EXISTS habits (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT UNIQUE,
+        object BLOB
+               
+    )''')
+    conn.commit()
+
+'''functions to load the data to and from the database'''
+
+
+def save_object_to_db(obj):
+    conn = sqlite3.connect('my_database.db')
+    cursor = conn.cursor()
+    name=obj.name #for easy of seaching of habits in the database 
+    serialized_data = serialize_object(obj)
+    cursor.execute('INSERT OR REPLACE INTO habits (name,object) VALUES (?,?)', (name,serialized_data,))
+    conn.commit()
+
+def load_object_from_db1(name):
+    conn = sqlite3.connect('my_database.db')
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM habits WHERE name = ?",(name,))
+    result = cursor.fetchone()
+    if result:
+        #print(result)
+        serialized_data = result[2]#here were mostly interested in the object from the data base 
+        a=deserialize_object(serialized_data)
+        return a
+    return None
+
+## to handle the deleting of habits from out database
+def delete_habit_from_database(name):
+    conn= sqlite3.connect('my_database.db')
+    cursor=conn.cursor()
+    cursor.execute("DELETE FROM habits where name = ?",(name,))
+    conn.commit()
+    print(f"habit:{name}, is deleted from our records " )
+
+
+# to handle selecting all objects from the database 
+def select_all(table_name):
+    conn= sqlite3.connect('my_database.db')
+    cursor=conn.cursor()
+    cursor.execute(f"SELECT * FROM {table_name}")
+    objects=[]
+    result=cursor.fetchall()
+    if result:
+        for i in result:
+            objects.append(deserialize_object(i[2]))
+    return objects
+
+
+'''functions to serialize the data and deserialize the data'''
+def serialize_object(obj):
+    return pickle.dumps(obj)
+
+def deserialize_object(serialized_data):
+    return pickle.loads(serialized_data)
